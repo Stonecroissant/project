52df4d0c65e2266205e4455842eb49b9f98cc76c
Stonecroissant <103129385+Stonecroissant@users.noreply.github.com>
2024-01-30 16:52:17 +0200

    5

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const button = document.getElementById('button')
+let nav = document.querySelector('.fixed-menu');
 
-function animation(){
-    button.style.color = "light green"
-}
+nav.AddEventListener('click', function event(){
+    let clickTarget = event.target;
+    let activebtn = document.querySelector('.active');
 
+    if (clickTarget.ClassList.contains('nav-link')){
+        clickTarget.ClassList,add('active');
+        activebtn.classList.remove('active');
+    }
+
+})
 
