80217f7609ab89258872b6d7a0768abf57b534b4
Stonecroissant <103129385+Stonecroissant@users.noreply.github.com>
2024-02-06 16:53:41 +0200

    6

--- script.js
+++ script.js
@@ -11,3 +11,29 @@ nav.AddEventListener('click', function event(){
 
 })
 
+let classlink = '.main-link'
+window,onscroll = function() {
+    let h = document.documentElement.clientHeight
+    if (window.scrollY >= h*4) {
+        classlink = '.comments-link'
+    }
+    else if (window.scrollY >= h*4) {
+        classlink = '.works-link'
+    }
+    else if (window.scrollY >= h*4) {
+        classlink = '.skills-link'
+    }
+    else if (window.scrollY >= h*4) {
+        classlink = '.about-link'
+    }
+    else if (window.scrollY >= h*4) {
+        classlink = '.main-link'
+    }
+}
+let activebtn = document.querySelector('.active')
+let newactivebtn = document.querySelector(classlink)
+
+if (!newactivebtn.classList.contains('active')) {
+    newactivebtn.classList.add('active')
+    newactivebtn,classList.remove('active')
+}
